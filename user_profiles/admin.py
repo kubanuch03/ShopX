28a7393c345e138f7293cd2267913aab20c061e1
zazaka71 <zazaka71@gmail.com>
2024-03-21 22:57:28 +0600

    fix bag/ add yasg

--- user_profiles/admin.py
+++ user_profiles/admin.py
@@ -6,6 +6,10 @@ admin.site.register(SellerProfile)
 
 @admin.register(CustomUser)
 class UserProfileAdmin(admin.ModelAdmin):
+<<<<<<< HEAD
+=======
+
+>>>>>>> c3ddf98e65291b92b1d304bd5998061c19a2adeb
     list_display = ['email_or_phone','is_active',"is_seller","username"]
     list_filter = ["is_active",'is_seller']
     search_fields = ["is_seller"]
