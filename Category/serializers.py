539858986935ba1f9faf84fe1a5c2273b5325a51
bekatur32 <103990593+bekatur32@users.noreply.github.com>
2024-03-21 16:46:27 +0300

    Update serializers.py

--- Category/serializers.py
+++ Category/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
 from .models import Category, PodCategory
 
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ("id", "name", "slug", "img")
-        read_only_fields = ("id", "slug")  # Поля, которые можно только читать
-
-
 class PodCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = PodCategory
         fields = ("id", "name", "category", "slug")
         read_only_fields = ("id", "slug")
+
+class CategorySerializer(serializers.ModelSerializer):
+    pod_categories = PodCategorySerializer(many=True)
+    class Meta:
+        model = Category
+        fields = ("id", "name", "slug", "img", "pod_categories")
+        read_only_fields = ("id", "slug") 
