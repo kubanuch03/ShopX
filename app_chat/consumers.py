f7db2ddb59bfd6fe5bb1301f99b7949fc9b69f53
zazaka71 <zazaka71@gmail.com>
2024-03-24 17:59:20 +0600

    update

--- app_chat/consumers.py
+++ app_chat/consumers.py
@@ -2,7 +2,6 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
 import json
 
-
 from user_profiles.models import CustomUser
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
@@ -18,18 +17,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Получение id комнаты из URL-адреса
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'room_%s' % self.room_name
-        print(f"Подключение+ к комнате {self.room_name}")
-        # Присоединение к комнате
-        #await self.join_room(self.room_name)
-
-
+        print(f"Подключение к комнате {self.room_name} пользователем {self.scope['user']}")
 
     async def disconnect(self, close_code):
         print("выходим disconnect")
         await self.close()
         await self.leave_room(self.room_group_name)
         print("disconnect вызван")
-
 
     async def leave_room(self, room_group_name):
         channel_layer = get_channel_layer()
@@ -38,34 +32,48 @@ class ChatConsumer(AsyncWebsocketConsumer):
             room_group_name,
             self.channel_name
         )
+
     async def receive(self, text_data):
         data = json.loads(text_data)
         print(data)
         message = data['message']
         sender_username = data['sender_username']
+        recipient_username = data.get('recipient_username')
 
-        # Проверяем наличие ключа 'recipient_username' в объекте data
-        if 'recipient_username' in data:
-            recipient_username = data['recipient_username']
-            room_slug = f"{sender_username}_{recipient_username}"
-
-            await self.get_or_create_room(sender_username, recipient_username)
-
-            await self.save_message(sender_username, room_slug, message)
-            await self.channel_layer.group_send(
-                room_slug,
-                {
-                    'type': 'chat_message',
-                    'message': message,
-                    'username': sender_username
-                }
-            )
+        if recipient_username:
+            # Отправить сообщение обоим участникам комнаты
+            await self.send_message_to_users(sender_username, recipient_username, message)
         else:
-            # Обрабатываем случай, когда recipient_username отсутствует в сообщении
+            # Обработать случай, когда recipient_username отсутствует в сообщении
             print("Получатель не указан")
 
+    async def send_message_to_users(self, sender_username, recipient_username, message):
+        room_sender = await self.get_or_create_room(sender_username, recipient_username)
+        await self.save_message(sender_username, room_sender.slug, message)
 
-    # Receive message from room group
+        # Отправляем сообщение в групповой канал, связанный с комнатой отправителя
+        await self.channel_layer.group_send(
+            room_sender.slug,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': sender_username
+            }
+        )
+
+        room_recipient = await self.get_or_create_room(recipient_username, sender_username)
+        await self.save_message(sender_username, room_recipient.slug, message)
+
+        # Отправляем сообщение в групповой канал, связанный с комнатой получателя
+        await self.channel_layer.group_send(
+            room_recipient.slug,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': sender_username
+            }
+        )
+
     async def chat_message(self, event):
         message = event['message']
         sender_username  = event['username']
@@ -76,12 +84,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'username': sender_username
         }))
 
-
     async def get_or_create_room(self, sender_username, recipient_username):
         sender = await sync_to_async(CustomUser.objects.get)(username=sender_username)
-        print("Sender:", sender)
         recipient = await sync_to_async(CustomUser.objects.get)(username=recipient_username)
-        print("Recipient:", recipient)
 
         existing_room = await sync_to_async(Room.objects.filter(users=sender).filter(users=recipient).first)()
         if existing_room:
@@ -91,27 +96,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
         room_slug = f"{sender_username}_{recipient_username}"
         try:
             new_room = await sync_to_async(Room.objects.create)(name=room_name, slug=room_slug)
-            print("New room created:", new_room)
             await sync_to_async(new_room.users.add)(sender, recipient)
-            print("Users added to room")
             return new_room
         except Exception as e:
             print(f"Error creating room: {e}")
             return None
 
-    async def save_message(self, sender_username, room_slug, message):
+    async def save_message(self, username, room, message):
         try:
-            user = await sync_to_async(CustomUser.objects.get)(username=sender_username)
-            room = await sync_to_async(Room.objects.get)(slug=room_slug)
+            user = await sync_to_async(CustomUser.objects.get)(username=username)
+            room = await sync_to_async(Room.objects.get)(slug=room)
             await sync_to_async(Message.objects.create)(user=user, room=room, content=message)
-
-            await self.channel_layer.group_send(
-            room_slug,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'username': sender_username
-            }
-        )
         except Exception as e:
             print(f"Error saving message: {e}")
