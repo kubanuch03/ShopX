b3fbd5e8ccd08bc198e2fe9ec9c294de68b2b8a0
zazaka71 <zazaka71@gmail.com>
2024-03-24 17:00:38 +0600

    cht

--- Shopx/settings.py
+++ Shopx/settings.py
@@ -203,7 +203,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-           "hosts": [(CHANNELS_REDIS_HOST, 6379)],
+           "hosts": [("redis", 6379)],
         },
     },
 }
