a6d481ee288ef0a55bdb01d737d3d2f449c376c1
zazaka71 <zazaka71@gmail.com>
2024-03-24 17:32:22 +0600

    update

--- Shopx/asgi.py
+++ Shopx/asgi.py
@@ -1,27 +1,28 @@
 import os
-import django
-from django.core.asgi import get_asgi_application
-
-# Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shopx.settings')
-
-# Initialize Django
-django.setup()
-
-# Get the ASGI application
-django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 from django.urls import path
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Shopx.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
 from app_chat.consumers import ChatConsumer
 
 application = ProtocolTypeRouter({
-    "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path('ws/<str:room_name>/', ChatConsumer.as_asgi()),
-        ])
-    ),
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
 
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+                path("ws/<str:room_name>/", ChatConsumer.as_asgi()),
+            ])
+        )
+    ),
 })
