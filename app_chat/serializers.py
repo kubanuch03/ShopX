35ad7fbeb97a227831f40d982423d4318a5b20d0
zazaka71 <zazaka71@gmail.com>
2024-03-27 11:36:05 +0600

    chat

--- app_chat/serializers.py
+++ app_chat/serializers.py
@@ -4,10 +4,10 @@ from app_chat.models import Room, Message
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ['name','slug','is_deleted']
+        fields = ['name','slug','user1','user2','is_deleted_user1','is_deleted_user2']
 
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = ['room','user','content','date_added',]
+        fields = ['room','user','content','date_added','is_deleted']
