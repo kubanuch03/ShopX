e41dbe4a60bc122838c7c687ad8c055acb29df6b
zazaka71 <zazaka71@gmail.com>
2024-04-01 11:19:08 +0600

    delete setting email daniar

--- Shopx/settings.py
+++ Shopx/settings.py
@@ -209,7 +209,7 @@ CHANNEL_LAYERS = {
 }
 
 # EMAIL_CONFIG
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = config("EMAIL_BACKEND")
 EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_PORT = config("EMAIL_PORT")
 EMAIL_USE_TLS = config("EMAIL_USE_TLS")
