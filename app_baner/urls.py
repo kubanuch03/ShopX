285d40ca1e53a7e6abe582a4f67e6e731a5c389e
sadyr7 <sadyr.top@gmail.com>
2024-05-14 13:56:30 +0600

    baner optimizations and code fixes

--- app_baner/urls.py
+++ app_baner/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import (
-    BanerCreateListView,
+    BanerCreateView,
     BanerDeleteView,
     BanerDetailView,
     BanerListView,
@@ -13,7 +13,7 @@ urlpatterns = [
     path("list/baner/",BanerListView.as_view(),name='list-baner'),
     path("detail/baner/<int:pk>/",BanerDetailView.as_view(),name='detail-baner'),
 
-    path("create/baner/",BanerCreateListView.as_view(),name='create-list-baner'),
+    path("create/baner/",BanerCreateView.as_view(),name='create-list-baner'),
     path("update/baner/<int:pk>/",BanerUpdateView.as_view(),name='update-baner'),
     path("delete/baner/<int:pk>/",BanerDeleteView.as_view(),name='delete-baner'),
 
--- app_baner/views.py
+++ app_baner/views.py
@@ -1,27 +1,40 @@
 from rest_framework import generics, permissions, response, status
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
 
 
 from.models import Baner
 from .serializers import BanerSerializer
 
 # User Permissions
-class BanerListView(generics.ListAPIView):
-    queryset = Baner.objects.all()
-    serializer_class = BanerSerializer
-    permission_classes = [permissions.AllowAny]
+class BanerListView(APIView):
+    def get(self, request):
+        queryset = Baner.objects.all()
+        serializer = BanerSerializer(queryset, many=True)
+        return response.Response(serializer.data)
+
+
 
 class BanerDetailView(generics.RetrieveAPIView):
     queryset = Baner.objects.all()
     serializer_class = BanerSerializer
     permission_classes = [permissions.AllowAny]
 
+    def get_object(self):
+        baner_id = self.kwargs.get('pk')
+        return get_object_or_404(self.queryset, pk=baner_id)
+
+
 
 # Admin Permissions
 
-class BanerCreateListView(generics.ListCreateAPIView):
+
+
+class BanerCreateView(generics.CreateAPIView):
     queryset = Baner.objects.all()
     serializer_class = BanerSerializer
     permission_classes = [permissions.IsAdminUser]
+
 
 
 class BanerUpdateView(generics.UpdateAPIView):
@@ -29,18 +42,17 @@ class BanerUpdateView(generics.UpdateAPIView):
     serializer_class = BanerSerializer
     permission_classes = [permissions.IsAdminUser]
 
-
+    def get_object(self):
+        baner_id = self.kwargs.get('pk')
+        return get_object_or_404(self.queryset, pk=baner_id)
 
 class BanerDeleteView(generics.DestroyAPIView):
     queryset = Baner.objects.all()
     serializer_class = BanerSerializer
     permission_classes = [permissions.IsAdminUser]
 
-    def destroy(self,request,*args,**kwargs):
-        baner_id = kwargs.get('pk')
-        try:
-            instance = self.get_object()
-            self.perform_destroy(instance)
-            return response.Response({"success": f"Банер id: {baner_id} успешно удалена!"}, status=status.HTTP_200_OK)
-        except:
-            return response.Response({"error":f"Не удалось найти Банер id: {baner_id}"})
+    def destroy(self, request, *args, **kwargs):
+        baner_id = self.kwargs.get('pk')
+        instance = get_object_or_404(self.queryset, pk=baner_id)
+        self.perform_destroy(instance)
+        return response.Response({"success": f"Банер id: {baner_id} успешно удалена!"}, status=status.HTTP_200_OK)
