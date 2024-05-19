dc4749ebcaa41f3e17c296b2b4e69404cc4a4580
kubanuch03 <zazaka71@gmail.com>
2024-05-19 11:37:59 +0600

    CACHES add settings

--- Shopx/settings.py
+++ Shopx/settings.py
@@ -206,7 +206,9 @@ CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
         'LOCATION': ['redis://localhost:6379/1'],
-
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
         
     }
 }
