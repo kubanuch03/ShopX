cb2bbf429d84025c8e7d740f198c69b7fb1bd70e
kubanuch03 <zazaka71@gmail.com>
2024-05-19 19:10:09 +0600

    delete/ yasg base url

--- Shopx/settings.py
+++ Shopx/settings.py
@@ -160,10 +160,10 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "usr/src/app/static/"
+STATIC_ROOT = "static/"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "usr/src/app/media/"
+MEDIA_ROOT = "media/"
 
 
 
