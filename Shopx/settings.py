3db38b00f5c02eab627cd2b8a269c5f986d7b545
kubanuch03 <zazaka71@gmail.com>
2024-06-07 14:16:11 +0600

    update

--- Shopx/settings.py
+++ Shopx/settings.py
@@ -169,7 +169,7 @@ MEDIA_URL = "/media/"
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': 'redis:redis:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
@@ -216,7 +216,7 @@ CHANNEL_LAYERS = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': ['redis://127.0.0.1:6379/1'],
+        'LOCATION': ['redis://redis:6379/1'],
         
     }
 }
